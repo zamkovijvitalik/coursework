7b0cf42cf15d16be3cd4ad76746f6e451f5aa412
neuutaa <zamkovij.vitalik@lll.kpi.ua>
2025-05-09 14:33:40 +0300

    fixed pages

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('core.urls')),
+    path('', views.index, name='home'),
+    path('menu/', views.menu, name='menu'),
+    path('about/', views.about, name='about'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
