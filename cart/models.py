862a065a6b8dbe07cdf06a34638366111be878cc
neuutaa <zamkovij.vitalik@lll.kpi.ua>
2025-05-25 18:58:57 +0300

    more info in orders

--- cart/models.py
+++ cart/models.py
@@ -31,12 +31,18 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Замовлення #{self.id} ({self.name})"
+    
+    @property
+    def total_price(self):
+        return sum(item.total_price for item in self.orderitem_set.all())
 
 # Товари в замовленні
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f"{self.product.name} × {self.quantity}"
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
+
