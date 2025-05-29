f7848c6b5729f318a464572895e3def9c140eaa0
neuutaa <zamkovij.vitalik@lll.kpi.ua>
2025-05-29 20:07:02 +0300

    final fixe

--- cart/models.py
+++ cart/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.conf import settings
 
-# Окрема модель товару
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -9,7 +8,6 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-# Елемент кошика
 class CartItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL)
     session_key = models.CharField(max_length=40, blank=True)
@@ -17,7 +15,6 @@ class CartItem(models.Model):
     quantity = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
 
-# Замовлення
 class Order(models.Model):
     name = models.CharField(max_length=150)
     user = models.ForeignKey(
@@ -37,7 +34,6 @@ class Order(models.Model):
     def total_price(self):
         return sum(item.total_price for item in self.orderitem_set.all())
 
-# Товари в замовленні
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
